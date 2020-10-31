2e5ff1836aad2594a2f3e40a6b50a41e7f43ff49
Amey More <amey8976@gmail.com>
2020-10-31 18:19:54 +0530

    chore: String constants moved to different file

---
+++ constants.py
@@ -0,0 +1,9 @@
+# Regex
+POPULARITY_QUERY_REGEX = "^(gt|lt|gte|lte|ne):([0-9]+|([0-9]+.[0-9]+))$"
+IMDB_SCORE_QUERY_REGEX = "^(gt|lt|gte|lte|ne):([0-9]{1}|([0-9]{1}.[0-9]+))$"
+
+# Messages
+RESOURCE_NOT_FOUND = "Resource not found"
+OPERATION_NOT_PERMITTED = "Operation not permitted"
+INCORRECT_CREDENTIALS = "Incorrect username or password"
+CREDENTIALS_NOT_VALID = "Could not validate credentials"
